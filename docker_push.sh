6a14c18d87c52d07f028d17a052e1f17aabc17cf
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2019-07-20 14:13:58 -0400

    Trying to get auth to work

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USER" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 docker push jrcichra/smartcar_test
