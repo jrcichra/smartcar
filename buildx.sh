52f4cc2b793331602776a021d78994670fab08f6
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2019-07-20 23:44:09 -0400

    remove slash

--- buildx.sh
+++ buildx.sh
@@ -2,6 +2,8 @@
 cd containers
 for i in $(ls -d */)
 do
+    #slice down the last slash
+    i=${i%/}
     cd $i
     docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t jrcichra/smartcar_$i --push .
     docker buildx imagetools inspect jrcichra/smartcar_$i
