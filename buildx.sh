f0f5ed68557aeff02c1be2d3bb2a2d34a6c32afd
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2019-07-20 23:34:41 -0400

    fix path

--- buildx.sh
+++ buildx.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-echo "PWD=$(pwd)"
-echo "Here's an ls..."
-ls
-echo "doing stuff now"
-for i in $(ls containers/)
+cd containers
+for i in $(ls -d)
 do
     cd $i
     docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t jrcichra/smartcar_$i --push .
     docker buildx imagetools inspect jrcichra/smartcar_$i
     cd ..
 done
+cd ..
