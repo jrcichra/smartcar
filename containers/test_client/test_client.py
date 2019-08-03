29e15baba05936709d82853130dc97b6126e093c
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2019-08-03 19:03:27 -0400

    Double registeration testing

--- containers/test_client/test_client.py
+++ containers/test_client/test_client.py
@@ -50,3 +50,52 @@ register_action = {
 
 s.sendall(packetize(json.dumps(register_action)))
 print(json.loads(depacketize(receive_packet(s)[0])))
+
+print("Phase 4 - Try to reregister a container that already exists - This should fail")
+
+register_container_again = {
+    'type': "register-container",
+    'timestamp': time.time(),
+    'data': {
+        'container': {
+            'container_id': socket.gethostname()
+        }
+    }
+}
+
+s.sendall(packetize(json.dumps(register_container_again)))
+print(json.loads(depacketize(receive_packet(s)[0])))
+
+print("Phase 5 - Try to reregister an event that already exists - This should fail")
+
+register_event_again = {
+    'type': "register-event",
+    'timestamp': time.time(),
+    'container_id': socket.gethostname(),
+    'data': {
+        'event': {
+            'name': "key_on"
+        }
+    }
+}
+
+s.sendall(packetize(json.dumps(register_event_again)))
+print(json.loads(depacketize(receive_packet(s)[0])))
+
+print("Phase 5 - Try to reregister an action that already exists - This should fail")
+
+register_action_again = {
+    'type': "register-action",
+    'timestamp': time.time(),
+    'container_id': socket.gethostname(),
+    'data': {
+        'action': {
+            'name': "killcar"
+        }
+    }
+}
+
+s.sendall(packetize(json.dumps(register_action_again)))
+print(json.loads(depacketize(receive_packet(s)[0])))
+
+print("Test Client Container Completed")
