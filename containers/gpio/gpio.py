cde0b69e3ca94827c6cc868b9b671f2306975604
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2019-09-10 19:11:08 -0400

    Try emitting an event

--- containers/gpio/gpio.py
+++ containers/gpio/gpio.py
@@ -1,7 +1,14 @@
 import smartcarsocket
+import time
 
 sc = smartcarsocket.smartcarsocket()
 sc.registerContainer()
 sc.registerEvent("key_on")
 sc.registerEvent("key_off")
 sc.registerAction("power_off")
+
+time.sleep(10)
+sc.emitEvent("key_on")
+
+time.sleep(10)
+sc.emitEvent("key_off")
