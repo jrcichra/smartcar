fab2d0f4d19125e9e514b47abcd6da45c2632857
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2019-10-06 19:28:20 -0400

    GPIO and Dashcam logic

--- containers/dashcam/dashcam.py
+++ containers/dashcam/dashcam.py
@@ -4,19 +4,66 @@ import queue
 import logging
 import time
 import os
-if os.uname()[4] == 'armv7l':
+import datetime
+
+FRAMERATE = 10           # Framerate used
+HRES = 1280  # Horizontal pixels
+VRES = 720  # Vertical pixels
+
+
+def isCI():
+    return os.uname()[4] != 'armv7l'
+
+
+camera = None
+##get_new_filename##
+
+
+def getserial():
+    # Extract serial from cpuinfo file
+    cpuserial = "0000000000000000"
+    try:
+        f = open('/proc/cpuinfo', 'r')
+        for line in f:
+            if line[0:6] == 'Serial':
+                cpuserial = line[10:26]
+        f.close()
+    except:
+        cpuserial = "ERROR000000000"
+
+    return cpuserial
+
+
+def get_new_filename():
+    return "travel__" + datetime.datetime.now().strftime("%Y--%m--%d__%H--%M--%S") + "__" + str(getserial()) + ".h264"
+
+
+if not isCI():
+    # Do all the camera setup, this will probably come from a config file at some point...
     import picamera
+    global camera
+    camera = picamera.PiCamera()  # the camera object
+    camera.resolution = (HRES, VRES)
+    # annotations
+    camera.annotate_foreground = picamera.Color('white')
+    camera.annotate_background = picamera.Color('black')
+    camera.annotate_frame_num = True
+    camera.annotate_text_size = 48
+    camera.annotate_text = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+    # set the framerate
+    camera.framerate = FRAMERATE
+    # set the rotation
+    camera.rotation = 0
+    camera.preview.alpha = 128
+    current_filename = get_new_filename()
+
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s.%(msecs)d:LINE %(lineno)d:TID %(thread)d:%(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 # Does an action based on the message we get. The calls needed are made by the container coder
 
 
-def performAction(msg, sc):
-    logging.debug("I got an action:")
-    logging.debug(msg)
-    # Act like we have some work to do for this
-    time.sleep(5)
+def sendResponse(msg, sc):
     # Craft a response with the actionResponse object
     response = sc.newActionResponse(msg['data']['name'])
     response.setEventID(msg['event_id'])
@@ -25,16 +72,76 @@ def performAction(msg, sc):
     sc.sendall(response)
 
 
+def start_preview(msg, sc):
+    if not isCI():
+        try:
+            global camera
+            camera.start_preview()
+        except Exception as e:
+            logging.error(e)
+    else:
+        logging.info("We're in CI, we would have started the preview")
+    sendResponse(msg, sc)
+
+
+def stop_preview(msg, sc):
+    if not isCI():
+        try:
+            global camera
+            camera.stop_preview()
+        except Exception as e:
+            logging.error(e)
+    else:
+        logging.info("We're in CI, we would have stopped the preview")
+    sendResponse(msg, sc)
+
+
+def start_recording(msg, sc):
+    if not isCI():
+        try:
+            global camera
+            camera.start_recording(get_new_filename(), sps_timing=True)
+        except Exception as e:
+            logging.error(e)
+    else:
+        logging.info("We're in CI, we would have started recording")
+    sendResponse(msg, sc)
+
+
+def stop_recording(msg, sc):
+    if not isCI():
+        try:
+            global camera
+            camera.stop_recording()
+        except Exception as e:
+            logging.error(e)
+    else:
+        logging.info("We're in CI, we would have stopped recording")
+    sendResponse(msg, sc)
+
 # Ideally we could get this into the library and not put it on the user? Not sure
+
+
 def getActions(sc, temp):
     while True:
         msg = sc.getQueue().get()
         if msg['type'] == "trigger-action":
             # Trigger the action
-            logging.debug("We got a trigger-action to do " +
-                          msg['data']['name'])
-            a = threading.Thread(target=performAction, args=(msg, sc))
-            a.start()
+            if msg['data']['name'] == 'start_recording':
+                a = threading.Thread(target=start_recording, args=(msg, sc))
+                a.start()
+            elif msg['data']['name'] == 'stop_recording':
+                a = threading.Thread(target=stop_recording, args=(msg, sc))
+                a.start()
+            elif msg['data']['name'] == 'start_preview':
+                a = threading.Thread(target=start_preview, args=(msg, sc))
+                a.start()
+            elif msg['data']['name'] == 'stop_preview':
+                a = threading.Thread(target=stop_preview, args=(msg, sc))
+                a.start()
+            else:
+                logging.error("I don't recognize this action:")
+                logging.error(msg)
         else:
             logging.warning(
                 "Got a packet response that wasn't what we expected, the library should handle this:")
--- containers/gpio/gpio.py
+++ containers/gpio/gpio.py
@@ -1,14 +1,147 @@
 import smartcarsocket
+import threading
+import queue
+import logging
 import time
+import os
 
+def isCI():
+    return os.uname()[4] != 'armv7l'
+
+if not isCI():
+    import RPi.GPIO as GPIO
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s.%(msecs)d:LINE %(lineno)d:TID %(thread)d:%(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+# Does an action based on the message we get. The calls needed are made by the container coder
+
+GREEN_LED = 5           # PIN 29 turn on green light on feather latchable relay board
+UNLATCH = 6             # PIN 31 used to unlatch the power relay (on feather board)
+                         #                The relay was latched with 12v power from 'key'
+KEY_OFF = 13             # PIN 33 used to see if the key is off
+KEY_ON = 19              # PIN 35 used to see if the key is on
+
+
+
+def sendResponse(msg, sc):
+    # Craft a response with the actionResponse object
+    response = sc.newActionResponse(msg['data']['name'])
+    response.setEventID(msg['event_id'])
+    response.setMessage("OK")
+    response.setStatus(0)
+    sc.sendall(response)
+
+def power_off(msg, sc):
+    # For now, I'm going to have it so the power_off function only works when the key is actually off
+    # if we run this code and find that the car is actually back on, we'll emit a key_on
+    if is_off():
+        # Okay, the key is off, we want to pull the relay
+        # first, sync the filesystem
+        os.system("sync")
+        # sleep a little
+        time.sleep(3)
+        # unlatch
+        if not isCI():
+            GPIO.output(UNLATCH,True)
+            # we shouldn't get here unless the unlatch is broken
+            time.sleep(5)
+            logging.error("We tried to shut off the car but it didn't work!!!")
+        else:
+            logging.info("This is where the car would be shut down, but we're in a CI environment")
+    else:
+        # The key isn't off? let the logs know
+        logging.error("Not shutting off the car because the key isn't off?")
+    # no matter what, send a response
+    sendResponse(msg,sc)
+
+
+# Ideally we could get this into the library and not put it on the user? Not sure
+def getActions(sc, temp):
+    while True:
+        msg = sc.getQueue().get()
+        if msg['type'] == "trigger-action":
+            # Trigger the action
+            logging.debug("We got a trigger-action to do " +
+                          msg['data']['name'])
+            if msg['data']['name'] == 'power_off':
+                a = threading.Thread(target=power_off, args=(msg, sc))
+                a.start()
+            else:
+                logging.warning("Got a trigger-action that I don't understand: printing for debugging:")
+                logging.warning(msg)
+        else:
+            logging.warning(
+                "Got a packet response that wasn't what we expected, the library should handle this:")
+            logging.info(msg)
+
+# Double check the key actually went off
+def is_off():
+  return GPIO.input(KEY_OFF) and not GPIO.input(KEY_ON)
+
+##print pins##
+def print_pins():
+  global KEY_OFF
+  global KEY_ON
+  
+  logging.info("##############################")
+  logging.info("KEY_OFF (PIN 33)="+str(GPIO.input(KEY_OFF)))
+  logging.info("KEY_ON (PIN 35)="+str(GPIO.input(KEY_ON)))
+  logging.info("##############################")
+
+def key_went_off(self):
+    logging.info("We got a change in key state...")
+    time.sleep(2)
+    print_pins()
+
+    if is_off():
+        logging.info("Yes, the key did in fact go off.")
+        # We emit the event
+        sc.emitEvent("key_off")
+    else:
+        #In some situations, we might want to send "key_on" here. Not doing this yet
+        logging.info("False alarm, or the owner turned the key back on within the timeout.")
+
+def gpio_setup():
+    if not isCI():
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(True)
+        # Set up output pins
+        GPIO.setup(GREEN_LED, GPIO.OUT)
+        GPIO.setup(UNLATCH, GPIO.OUT)
+        # Set up input pins
+        GPIO.setup(KEY_OFF, GPIO.IN,pull_up_down=GPIO.PUD_UP)  #Used to check if ignition is on
+        #event handling for when KEY_OFF is triggered
+        GPIO.add_event_detect(KEY_OFF, GPIO.RISING, callback=key_went_off, bouncetime=500)
+        GPIO.setup(KEY_ON,  GPIO.IN,pull_up_down=GPIO.PUD_UP)  #Used to check if ignition is onclear
+    else:
+        logging.info("We're in the CI, no GPIO setup. Sleeping for 10 seconds, and then pretending the key went off.")
+        time.sleep(10)
+        key_went_off(None)
+
+
+#MAIN#
+
+# Use the library to abstract the difficulty
 sc = smartcarsocket.smartcarsocket()
+
+# Register ourselves and what we provide to the environment
+sc.registerContainer()
+
 sc.registerContainer()
 sc.registerEvent("key_on")
 sc.registerEvent("key_off")
 sc.registerAction("power_off")
 
-time.sleep(10)
+# Handle incoming action requests
+t = threading.Thread(target=getActions, args=(sc, True))
+t.start()
+
+# If this code is running, the key must be on, so we'll force a key_on event
+# Change if your pi doesn't start with the car.
 sc.emitEvent("key_on")
 
-time.sleep(10)
-sc.emitEvent("key_off")
+# Set up the GPIO pins if we're not in travis
+gpio_setup()
+
+
+t.join()
