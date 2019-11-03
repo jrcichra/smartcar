5040caca15ad1fa1adee77c53a9e7203db5b10bf
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2019-11-02 23:15:58 -0400

    no -it on commit check

--- check_commit.sh
+++ check_commit.sh
@@ -1,5 +1,5 @@
 cd containers
 for d in */; do
     dir=${d%/}
-    test $(docker run -it --rm jrcichra/smartcar_$dir cat commit.txt) = $GITHUB_SHA
+    test $(docker run --rm jrcichra/smartcar_$dir cat commit.txt) = $GITHUB_SHA
 done
