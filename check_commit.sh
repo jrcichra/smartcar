13d4f4277068370e6c704f2af7f6347324b89987
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2019-11-04 17:41:21 -0500

    use {} to fix variables

--- check_commit.sh
+++ check_commit.sh
@@ -2,7 +2,7 @@ cd containers
 if [ "$1" == "rpi" ];then
     for d in */; do
         dir=${d%/}
-        test $(docker run --rm jrcichra/smartcar_$dir_rpi cat commit.txt) = $GITHUB_SHA
+        test $(docker run --rm jrcichra/smartcar_${dir}_rpi cat commit.txt) = $GITHUB_SHA
     done
 else
     for d in */; do
