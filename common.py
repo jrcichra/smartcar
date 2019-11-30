47bdaa69d5e88207fb36016df900a4ae7eb456d2
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2019-11-30 00:00:59 -0500

    Use get to avoid a key error

--- common.py
+++ common.py
@@ -2,7 +2,7 @@ import os
 
 
 def isCI():
-    res = os.environ['GITHUB_ACTIONS']
+    res = os.environ.get('GITHUB_ACTIONS')
     if res is not None:
         print("I just did a check and I am indeed in a CI environment.")
     return res
