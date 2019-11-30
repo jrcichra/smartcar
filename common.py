f52b3e2abe29808e6e7b65261c5d818c0861bb3e
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2019-11-30 09:42:19 -0500

    try equals instead of is

--- common.py
+++ common.py
@@ -4,7 +4,7 @@ import os
 def isCI():
     res = os.environ.get('GITHUB_ACTIONS')
     print("isCI = {}".format(res))
-    if res is None:
+    if res == None:
         return False
     else:
         return True
