8ae4a650503d60460dde21d5dba5e97dc11870cf
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2019-12-01 23:08:48 -0500

    another condition for is None

--- common.py
+++ common.py
@@ -3,7 +3,7 @@ import os
 
 def isCI():
     res = os.environ.get('GITHUB_ACTIONS')
-    if res == None or res == "":
+    if res == None or res == "" or res is None:
         return False
     else:
         return True
