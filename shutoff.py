758a042e3aa1515660f838c7d44536a11bf6966f
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2019-12-04 23:17:16 -0500

    add script to shut down the pi

---
+++ shutoff.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import os
+import time
+import RPi.GPIO as GPIO
+
+# PIN 31 used to unlatch the power relay (on feather board)
+UNLATCH = 6
+
+
+def power_off():
+    print("Shutting down the system in a few seconds...")
+    # first, sync the filesystem (output before sync so things are more likely to be saved)
+    os.system("sync")
+    # sleep a little
+    time.sleep(3)
+    # unlatch
+    GPIO.output(UNLATCH, True)
+
+
+power_off()
