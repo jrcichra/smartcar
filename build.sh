a30a06881ef7a41e6d92bdb1831477cd5d0934b1
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2020-03-25 00:26:26 -0400

    missing a space, removing old elm

--- build.sh
+++ build.sh
@@ -58,7 +58,7 @@ else
             docker buildx build --cache-from jrcichra/smartcar_${dir} --platform linux/amd64,linux/arm64,linux/arm/v7 -t jrcichra/smartcar_${dir}:${GITHUB_SHA:0:8} --push .
             docker buildx imagetools inspect jrcichra/smartcar_${dir}:${GITHUB_SHA:0:8}
             # Also update :latest (should be quick since we just built)
-            docker buildx build --cache-from jrcichra/smartcar_${dir}--platform linux/amd64,linux/arm64,linux/arm/v7 -t jrcichra/smartcar_${dir}:latest --push .
+            docker buildx build --cache-from jrcichra/smartcar_${dir} --platform linux/amd64,linux/arm64,linux/arm/v7 -t jrcichra/smartcar_${dir}:latest --push .
             docker buildx imagetools inspect jrcichra/smartcar_${dir}:latest
             cd ..
         fi
