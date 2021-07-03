208f1dd0dc30f881ff073f40f44b6000c2cb9ba2
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2021-07-02 21:30:46 -0400

    fix syntax, host runner finds host

--- containers/host_runner/host_runner.py
+++ containers/host_runner/host_runner.py
@@ -5,6 +5,7 @@ import logging
 import time
 import os
 import datetime
+import socket
 from common import isCI
 
 logging.basicConfig(level=logging.DEBUG,
@@ -13,8 +14,12 @@ logging.basicConfig(level=logging.DEBUG,
 
 def execute(params, result):
 
+    # hopefully we get the docker network we're on and we want to talk to the host
+    hostname_array = socket.gethostbyname(socket.gethostname()).split('.')
+    hostname_array[3] = '1'
+    hostname = '.'.join(hostname_array)
+
     command = params['command']
-    hostname = params['hostname']
     username = params['username']
     password = params['password']
 
@@ -25,6 +30,7 @@ def execute(params, result):
         result.Fail()
 
 ###MAIN###
+
 
 # Use the library to abstract the difficulty
 k = karmen.Client()
